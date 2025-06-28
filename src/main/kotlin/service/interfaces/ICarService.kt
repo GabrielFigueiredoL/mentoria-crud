7f0ab0459dc8a3b83f3dfc0ca0925d4e846b7c38
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2025-06-28 11:24:25 -0300

    refactor: remove blank space

--- src/main/kotlin/service/interfaces/ICarService.kt
+++ src/main/kotlin/service/interfaces/ICarService.kt
@@ -3,7 +3,7 @@ package org.gabrielfigueiredol.service.interfaces
 import org.gabrielfigueiredol.domain.Car
 
 interface ICarService {
-    fun registerCar(car : Car);
+    fun registerCar(car: Car);
     fun getCar(id: Int);
     fun getAllCars();
     fun updateCar(id: Int, updatedCar: Car);
